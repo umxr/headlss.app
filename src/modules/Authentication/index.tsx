0cf41cb13f520fdbccfbbae9ec5dca3c2451df87
umxr <umarg1997@gmail.com>
2020-07-24 21:48:16 +0100

    Authentication Module

---
+++ src/modules/Authentication/index.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import {
+  Box,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/core";
+
+import { CustomerContext } from "../../config/context/createCustomerContext";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Authentication = ({ children }: Props) => {
+  const { customerAccessToken } = useContext(CustomerContext);
+  if (!customerAccessToken) {
+    return (
+      <Box p={6}>
+        <Alert __css={{}} status="error">
+          <AlertIcon />
+          <AlertTitle mr={2}>Unauthorised!</AlertTitle>
+          <AlertDescription>
+            Seems like you dont have access to this page.
+          </AlertDescription>
+        </Alert>
+      </Box>
+    );
+  }
+
+  return children;
+};
+
+export default Authentication;
--- src/templates/account/Dashboard/containers/DashboardContainer/index.tsx
+++ src/templates/account/Dashboard/containers/DashboardContainer/index.tsx
@@ -5,23 +5,26 @@ import Layout from "../../../../../modules/Layout";
 import DashboardDetails from "../../components/DashboardDetails";
 import DashboardPreferences from "../../components/DashboardPreferences";
 import DashboardAddress from "../../components/DashboardAddress";
+import Authentication from "../../../../../modules/Authentication";
 
 const DashboardContainer = () => {
   return (
     <Layout>
-      <SimpleGrid
-        p={6}
-        columns={{
-          md: 1,
-          lg: 2,
-          xl: 3,
-        }}
-        spacing={6}
-      >
-        <DashboardDetails />
-        <DashboardPreferences />
-        <DashboardAddress />
-      </SimpleGrid>
+      <Authentication>
+        <SimpleGrid
+          p={6}
+          columns={{
+            md: 1,
+            lg: 2,
+            xl: 3,
+          }}
+          spacing={6}
+        >
+          <DashboardDetails />
+          <DashboardPreferences />
+          <DashboardAddress />
+        </SimpleGrid>
+      </Authentication>
     </Layout>
   );
 };
